88720546df6c917ef0391f38d08e78ec0b253361
Xutik <kseniia.prakhova@hyperisland.se>
2024-09-15 22:23:25 +0200

    Final: Start! Play. Create a table

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const n = 4;
-const max = Math.pow(n, 2)
+let n = 16;
 const size = 960;
 
-const table = document.querySelector('#table');
+function createTable(){
+  table.innerHTML = '';
+  
+  const max = Math.pow(n, 2)
 
-for (let i = 1; i < max + 1; i++) {
-  const h = 960 / n;
-  const w = 960 / n;
-  const childDiv = document.createElement('div');
-  childDiv.style.width = w + 'px';
-  childDiv.style.height = h + 'px';
-  table.appendChild(childDiv);
+  for (let i = 1; i < max + 1; i++) {
+    const h = 960 / n;
+    const w = 960 / n;
+    const childDiv = document.createElement('div');
+    childDiv.style.width = w + 'px';
+    childDiv.style.height = h + 'px';
+    childDiv.addEventListener('mouseover', () => {
+      childDiv.style.backgroundColor = 'green'
+    })
+    childDiv.addEventListener('mouseleave', () => {
+      childDiv.style.backgroundColor = 'brown'
+    })
+  
+    table.appendChild(childDiv);
+  }
 }
 
+const table = document.querySelector('#table');
+const button = document.querySelector('#idbtn');
+
+button.addEventListener('click', () => {
+  n = prompt("Type number (<100) to create a new table");
+  n = parseInt(n);
+  createTable();
+});
+
+createTable();
